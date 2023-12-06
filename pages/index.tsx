8f563be3fce90c5604f50d7af13b4fda70448750
Hassnain Kureshy <hassnainkureshy@gmail.com>
2023-12-06 07:23:57 -0800

    initial commit

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Hassnain Kureshy</title>
+        <meta name="description" content="Welcome!" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <section>
+        <h1>Hassnain Kureshy</h1>
+      </section>
+    </>
+  )
+}
