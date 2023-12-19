51d1c41a339ac55a83907c570ae46dd7ed3500ad
Hassnain Kureshy <hassnainkureshy@gmail.com>
2023-12-19 08:31:42 -0800

    updated readme

--- components/about/Description.tsx
+++ components/about/Description.tsx
@@ -1,6 +1,6 @@
 export const Description = () => (
   <p className='mt-4 text-gray-600'>
-    Dynamic Full Stack Developer with over 7 years of experience in developing scalable web applications and managing the full
+    Dynamic Full Stack Developer with over 8 years of experience in developing scalable web applications and managing the full
     software development lifecycle. Proven ability to adapt to new technologies and deliver user-centric solutions. Eager to
     contribute to a forward-thinking team to drive growth and technological innovation.
   </p>
